688db6ae65808b71c4bf393b5e8f0097e4494f55
michiboo <chanmickyyun@gmail.com>
2020-11-16 06:34:24 +0000

    format carosel

--- frontend/hk_stock_web/src/App.js
+++ frontend/hk_stock_web/src/App.js
@@ -2,19 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import SearchAppBar from './components/SearchAppBar'
 import {Make_caro} from './components/Carousel'
-// constructor() {
-//   this.Make_caro = this.Make_caro.bind(this);
-// }
+
+var left_caro_sty={style : { width:'50%', display: 'inline-block', height:'200%'}}
+var right_caro_sty={style : { width:'50%', float:'right'}}
+var top_caro_sty={style : { width:'100%'}}
 
 function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-      PlaceHolder
-      </header>
       <SearchAppBar/>
-      {Make_caro()}
+      {Make_caro(top_caro_sty)}
+      {Make_caro(left_caro_sty)}
+      {Make_caro(right_caro_sty)}
     </div>
   );
 }
--- frontend/hk_stock_web/src/components/Carousel.js
+++ frontend/hk_stock_web/src/components/Carousel.js
@@ -1,36 +1,39 @@
 import React from 'react';
 import Carousel from 'react-material-ui-carousel'
-import {Paper} from '@material-ui/core'
+import {Paper, Button} from '@material-ui/core'
  
 export const  Make_caro = (props) =>
-{
+{   
     var items = [
         {
             name: "Random Name #1",
-            description: "Probably the most random thing you have ever seen!"
+            description: "Probably the most random thing you have ever seen!",
+            style: {height: '20vh'},
         },
         {
             name: "Random Name #2",
-            description: "Hello World!"
+            description: "Hello World!",
+            style: {height: '20vh'},
         }
     ]
- 
+    
     return (
+        <div style={props.style}>
         <Carousel>
             {
                 items.map( (item, i) => <Item key={i} item={item} /> )
             }
         </Carousel>
+        </div>
     )
 }
  
 function Item(props)
 {
     return (
-        <Paper>
+        <Paper style={props.item.style}>
             <h2>{props.item.name}</h2>
             <p>{props.item.description}</p>
- 
             <Button className="CheckButton">
                 Check it out!
             </Button>
