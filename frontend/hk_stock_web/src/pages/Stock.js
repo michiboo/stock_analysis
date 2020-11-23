5de8fd2274f041f2026d25cb8cfa26f397ead68c
michiboo <chanmickyyun@gmail.com>
2020-11-23 01:37:56 +0000

    able to get result for axios

--- frontend/hk_stock_web/src/pages/Stock.js
+++ frontend/hk_stock_web/src/pages/Stock.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import TimeLineGraph from '../components/TimeLineGraph';
+import axios from 'axios';
 
 
-export default function Stock() {
-    return (
-        <div>
-            <div style={{ position: "relative", width: '20%' }}><TimeLineGraph /></div>
-        </div>
-    );
+export default class Stock extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            stock_price: [],
+        };
+    }
+    componentDidMount() {
+        var _this = this;
+        axios.get('https://www.quandl.com/api/v3/datasets/OPEC/ORB.json?api_key=s8a7SZvi5CWLMWMQAnQ8').then(function (res) {
+            _this.setState({
+                stock_price: res.data.dataset
+            });
+        })
+            .catch(function (e) {
+                console.log("ERROR ", e);
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <div style={{ position: "relative", width: '20%' }}><TimeLineGraph /></div>
+                <div>hello</div>
+                <ul>
+                {this.state.stock_price ? (this.state.stock_price.data ? this.state.stock_price.data.map((reptile) => <li>{reptile[1]}</li>) : 'data null') : 'Object still NULL'}
+                </ul>
+            </div>
+        );
+    }
 }
